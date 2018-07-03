bc6f15e504b4ba55698546f8e159c1b44058fa57
songy <songy@aicaigroup.com>
2018-07-03 15:28:23 +0800

    react-native firstApp

---
+++ android/app/src/main/java/com/firstapp/ToastModule.java
@@ -0,0 +1,45 @@
+package com.firstapp;
+
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+/**
+ * Description:
+ * Created by song on 2018/7/3.
+ * email：bjay20080613@qq.com
+ */
+public class ToastModule  extends ReactContextBaseJavaModule {
+    private static final String TAG="ToastModule";
+    private static final String DURATION_SHORT_KEY="SHORT";
+    private static final String DURATION_LONG_KEY="LONG";
+    public ToastModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "ToastExample";
+    }
+
+    @Nullable
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String,Object> constants=new HashMap<>();
+        constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+        constants.put(DURATION_LONG_KEY,Toast.LENGTH_LONG);
+        return constants;
+    }
+
+    @ReactMethod
+    public void show(String message,int duration){
+        Toast.makeText(getReactApplicationContext(),message,duration).show();
+    }
+}
